a1f033e60264fb0012b1456c83cd058315a9c310
damienCHAPEAU <damien.chapeau81@gmail.com>
2022-10-17 19:35:19 +0900

    WIP Theme

--- src/theme/BaseTheme.ts
+++ src/theme/BaseTheme.ts
@@ -1,31 +1,174 @@
-import { createTheme } from '@mui/material/styles';
+import { alpha, createTheme } from '@mui/material/styles';
 
 /*
-    * Base theme
-    * declare the base theme overrides here
-*/
+ * Base theme
+ * declare the base theme overrides here
+ */
 declare module '@mui/material/styles' {
-    // interface name and interface type
+  // interface name and interface type
 }
-    
+
 declare module '@mui/material/Chip' {
-    interface ChipPropsVariantOverrides {
-        // setup new variant
+  interface ChipPropsVariantOverrides {
+    // setup new variant
     //   text: true;
-    }
   }
-  
-  declare module '@mui/material/Button' {
-    interface ButtonPropsColorOverrides {}
-    interface ButtonPropsVariantOverrides {
-        // expand the variant
+}
+
+declare module '@mui/material/TextField' {
+  interface TextFieldPropsVariantOverrides {
+    // setup new variant
+    search: true;
+  }
+}
+
+declare module '@mui/material/Checkbox' {
+  interface CheckboxPropsVariantOverrides {
+    // setup new variant
+    //   text: true;
+  }
+}
+
+declare module '@mui/material/Button' {
+  interface ButtonPropsColorOverrides {}
+  interface ButtonPropsVariantOverrides {
+    // expand the variant
     //   circleExpand: true;
-    }
   }
+}
 
 // Create a theme instance.
 export const BaseTheme = createTheme({
-
+  typography: {
+    fontFamily: 'Montserrat',
+    h1: {
+      fontSize: 48,
+      fontWeight: 600,
+    },
+    h2: {
+      fontSize: 39,
+      fontWeight: 600,
+    },
+    h3: {
+      fontSize: 31,
+      fontWeight: 600,
+    },
+    h4: {
+      fontSize: 25,
+      fontWeight: 600,
+    },
+    h5: {
+      fontSize: 20,
+      fontWeight: 600,
+    },
+    h6: {
+      fontSize: 16,
+      fontWeight: 600,
+    },
+    body1: {
+      fontSize: 12,
+    },
+    body2: {
+      fontSize: 10,
+    },
+    button: {
+      fontSize: 17,
+      fontWeight: 700,
+      textTransform: 'none',
+    },
+  },
+  shape: {
+    borderRadius: 12,
+  },
+  spacing: [8, 16, 24, 32, 40, 48, 56, 64, 72, 80, 88, 96, 104, 112, 120],
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: ({ ownerState, theme }) => ({
+          padding: '20px 32px',
+          height: 50,
+          boxShadow: 'none',
+          //ownerState primary/contained
+          ...(ownerState.variant === 'contained' &&
+            ownerState.color === 'primary' && {
+              '&:hover': {
+                backgroundColor: theme.palette.secondary.main,
+                boxShadow: 'none',
+              },
+              '&:focus': {
+                backgroundColor: theme.palette.secondary.main,
+                boxShadow: `0px 0px 0px 2px ${theme.palette.inactive.main}`,
+              },
+              '&:active': {
+                backgroundColor: theme.palette.primary.dark,
+                boxShadow: 'none',
+              },
+            }),
+          ...(ownerState.variant === 'outlined' &&
+            ownerState.color === 'primary' && {
+              border: 'none',
+              boxShadow: `0px 0px 0px 1px ${theme.palette.primary.main}`,
+              color: theme.palette.primary.main,
+              '&:hover': {
+                backgroundColor: `${theme.palette.secondary.main}50`,
+                boxShadow: 'none',
+                border: 'none',
+              },
+              '&:focus': {
+                backgroundColor: `${theme.palette.secondary.main}50`,
+                boxShadow: `0px 0px 0px 2px ${theme.palette.primary.main}`,
+              },
+              '&:active': {
+                backgroundColor: `${theme.palette.secondary.main}`,
+                color: theme.palette.primary.contrastText,
+                boxShadow: 'none',
+                border: 'none',
+              },
+            }),
+          //disabled
+          '&.Mui-disabled': {
+            backgroundColor: theme.palette.disabled.main,
+          },
+        }),
+      },
+    },
+    MuiTextField: {
+      styleOverrides: {
+        root: ({ ownerState, theme }) => ({
+          '& .MuiOutlinedInput-root': {
+            backgroundColor: theme.palette.disabled.main,
+            '&:hover': {
+              boxShadow: `${alpha(theme.palette.primary.main, 1)} 0 0 0 1px`,
+            },
+            '& fieldset': {
+              border: 'none',
+            },
+            '&.Mui-focused': {
+              backgroundColor: theme.palette.primary.contrastText,
+              boxShadow: `${alpha(theme.palette.primary.main, 1)} 0 0 0 1px`,
+            },
+          },
+        }),
+      },
+    },
+    // MuiCheckbox: {
+    //   styleOverrides: {
+    //     root: ({ ownerState, theme }) => ({
+    //       color:
+    //         ownerState.color === 'error'
+    //           ? theme.palette.error.main
+    //           : theme.palette.inactive.main,
+    //       '&:hover': {
+    //         color: theme.palette.primary.main,
+    //       },
+    //       '&.Mui-checked': {},
+    //       '&.Mui-disabled': {
+    //         color: theme.palette.disabled.main,
+    //       },
+    //     }),
+    //   },
+    // },
+  },
 });
 
 export default BaseTheme;
--- src/theme/LightTheme.ts
+++ src/theme/LightTheme.ts
@@ -1,22 +1,59 @@
 import { createTheme, ThemeOptions } from '@mui/material/styles';
 import BaseTheme from './BaseTheme';
 
-const lightTheme: ThemeOptions = {
-    palette: {
-        mode: 'light',
-        primary: {
-            main: '#f44336',
-        },
-        secondary: {
-            main: '#3ea6ff',
-        },
-        background: {
-            default: '#fff',
-            paper: '#fff',
-        },
-    }
+declare module '@mui/material/styles' {
+  interface Palette {
+    disabled: Palette['primary'];
+    inactive: Palette['primary'];
+  }
+
+  interface PaletteOptions {
+    disabled: PaletteOptions['primary'];
+    inactive: PaletteOptions['primary'];
+  }
 }
 
+const lightTheme: ThemeOptions = {
+  palette: {
+    mode: 'light',
+    primary: {
+      main: '#FF7110',
+      dark: '#CE5900',
+      contrastText: '#fff',
+    },
+    secondary: {
+      main: '#FF9441',
+    },
+    success: {
+      main: '#38BF7D',
+    },
+    error: {
+      main: '#FF7070',
+    },
+    info: {
+      main: '#4271FD',
+    },
+    warning: {
+      main: '#EFC900',
+    },
+    disabled: {
+      main: '#F5F5F5',
+    },
+    inactive: {
+      main: '#A0A0A0',
+    },
+    background: {
+      default: '#fff',
+      paper: '#fff',
+    },
+    text: {
+      primary: '#161616',
+      secondary: '#7C7C7C',
+      disabled: '#A0A0A050',
+    },
+  },
+};
+
 // Create a theme instance.
-export const LightTheme = createTheme(BaseTheme, lightTheme)
+export const LightTheme = createTheme(BaseTheme, lightTheme);
 export default LightTheme;
